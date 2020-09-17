e8c678907f7188c6a87db91d9724035e094d4406
RafaelVulpe <vulpe.rafael@gmail.com>
2020-09-17 07:54:27 -0300

    redirects

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -27,10 +27,7 @@ export default function Header() {
             </div>
 
             <img
-              src={
-                profile.avatar.url ||
-                'https://api.adorable.io/avatars/50/abott@adorable.png'
-              }
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
               alt="Rafael Vulpe"
             />
           </Profile>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://104.131.19.44',
+  baseURL: 'http://104.131.19.44:3333',
 });
 
 export default api;
